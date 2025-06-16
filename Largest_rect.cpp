530a60a8fb883a58692418ec60083746d98d3510
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-06-16 13:34:38 +0530

    .

--- Largest_rect.cpp
+++ Largest_rect.cpp
@@ -1,57 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-        vector<int> largestRectangleArea(vector<int>& heights) {
-        vector<int>mp;
-        stack<int>st;
-        for (int i = heights.size()-1; i >= 0; i--) {
-  
-            int num = heights[i];
-            int width = 1;
-            while(!st.empty() && st.top() < num){ // if stack is not empty and top is not 0 and top is less than num then pop
-                st.pop();
-            }
-            if(st.empty()){
-                int area = width * num;
-                mp.push_back(area);
-            }
-            else{
-                if(heights[i]<heights[i-1] && i>0){
-                width = st.size()+1;
-                int area = width * num;
-                mp.push_back(area);
-            }
-            else{
-                    width = st.size()+1;
-                    int area = width * num;
-                    mp.push_back(area);
-                    for(int j = 0; j < st.size(); j++){
-                        st.pop();
-                    }
-                }
-            }
-            st.push(num);
-            
-        }
-        int ans = 0;
-        for(int i = 0; i < mp.size(); i++){
-            ans = max(ans, mp[i]);
-        }
-        return mp;
-    }
+    // int largestRectangleArea(vector<int> &heights)
+    // {
+    //     vector<int> mp;
+    //     stack<int> st;
+    //     int width  = st.size() + 1;
+    //     for (int i = heights.size() - 1; i >= 0; i--)
+    //     {
+    //         if (!st.empty() && st.top() < heights[i])
+    //         {   
+    //             int num = heights[i];
+    //             while (!st.empty() && st.top() < num)
+    //             {
+    //                 st.pop();
+    //                 width = 1;
+    //             }
+    //             int area = width * num;
+    //             mp.push_back(area);
+    //             st.push(num);
+    //         }
+    //         else
+    //         {
+    //             if (st.empty())
+    //             {
+    //                 int area = width * heights[i];
+    //                 mp.push_back(area);
+    //             }
+    //             else
+    //             {
+    //                 width = st.size() + 1;
+    //                 int area = width * heights[i];
+    //                 mp.push_back(area);
+    //             }
+    //             st.push(heights[i]);
+    //         }
+    //     }
+    //     int ans = 0;
+    //     for (int i = 0; i < mp.size(); i++)
+    //     {
+    //         ans = max(ans, mp[i]);
+    //     }
+    //     return ans;
+    // }
 };
 
-int main(){
+int main()
+{
     //[2,1,5,6,2,3]
-    vector<int>nums = {2,0,2};
+    vector<int> nums = {2,1,5,6,2,3};
     Solution s;
     // cout<<s.largestRectangleArea(nums)<<endl;
-    vector<int>ans = s.largestRectangleArea(nums);
-    for(int i = 0; i < ans.size(); i++){
-        cout<<ans[i]<<" ";
-    }
-  
+
     return 0;
-}   
+}
